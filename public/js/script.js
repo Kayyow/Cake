e44ad23ec50997178ae49d86f383efcae266ddb5
Benjamin Maigné <Benjamin Maign>
2016-05-11 20:54:59 +0200

    Fix : progress bar align and value in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -41,4 +41,75 @@ $(function() {
         var dom = '<div class="skill"><p>'+s.name+'</p><div class="bar"><div class="progress" style="width:'+s.percentage+'%"></div></div></div>'
         $('.reseau_skill').append(dom)
     }
+
+    var skills = [
+        {
+            name: "Bootstrap",
+            percentage: 25
+        },
+        {
+            name: "Html/Css",
+            percentage: 95
+        },
+        {
+            name: "php",
+            percentage: 50
+        },
+        {
+            name: "Javascript",
+            percentage: 80
+        },
+        {
+            name: "Symfony 2/ Bootstrap 3",
+            percentage: 40
+        }
+    ]
+
+    for (var i = 0; i < skills.length; i++) {
+        var s = skills[i]
+        var dom = '<div class="skill"><p>'+s.name+'</p><div class="bar"><div class="progress" style="width:'+s.percentage+'%"></div></div></div>'
+        $('.web_skill').append(dom)
+    }
+
+    var skills = [
+        {
+            name: "c#",
+            percentage: 10
+        },
+        {
+            name: "Langage C",
+            percentage: 50
+        },
+        {
+            name: "Java",
+            percentage: 40
+        }
+    ]
+
+    for (var i = 0; i < skills.length; i++) {
+        var s = skills[i]
+        var dom = '<div class="skill"><p>'+s.name+'</p><div class="bar"><div class="progress" style="width:'+s.percentage+'%"></div></div></div>'
+        $('.dev_skill').append(dom)
+    }
+
+    var skills = [
+        {
+            name: "Gestion de projet",
+            percentage: 35
+        },
+        {
+            name: "Architecture des ordinateurs",
+            percentage: 60
+        },
+        {
+            name: "Anglais",
+            percentage: 20
+        }
+    ]
+
+    for (var i = 0; i < skills.length; i++) {
+        var s = skills[i]
+        var dom = '<div class="skill"><p>'+s.name+'</p><div class="bar"><div class="progress" style="width:'+s.percentage+'%"></div></div></div>'
+        $('.other_skill').append(dom)
+    }
 });
